d2416b6c842bd6d6649494137348b1e617f0e285
ankparik <ankparik@cisco.com>
2017-03-19 04:50:13 -0400

    added dinner data for mealsDelivered

---
+++ mealsDelivered/js/availableMeals.js
@@ -0,0 +1,59 @@
+function availableDinner() {
+
+
+	var dinner = [
+
+	/*
+		{
+			"name"			: 		"",
+			"image"			: 		"",
+			"description"	: 		"",
+			"orderText"		: 		"",
+			"cost"			: 		,
+			"costText"		: 		"",
+			"notesText"		: 		""
+		}
+	*/
+		{
+			"name"			: 		"Roti",
+			"image"			: 		"http://www.comicoast.com/honest-ecommerce-website/product/img/roti.png",
+			"description"	: 		"Roti (also known as chapati) is a flatbread, made from stoneground wholemeal flour with a light layer of Ghee",
+			"orderText"		: 		"We recommend 3 to 4 rotis per person",
+			"cost"			: 		0.50,
+			"costText"		: 		"per roti",
+			"notesText"		: 		"Anything special? Like no Ghee, etc..."
+		},
+
+		{
+			"name"			: 		"Bhinda-Bataka nu Shaak",
+			"image"			: 		"http://4.bp.blogspot.com/-l20XGIEH7aA/TjYOFtLCrII/AAAAAAAARpI/PizQYlPv7NE/s1600/bhinda+vadi+shaak.JPG",
+			"description"	: 		"Okra and Potato cooked in Indian spices",
+			"orderText"		: 		"",
+			"cost"			: 		2.50,
+			"costText"		: 		"per person",
+			"notesText"		: 		""
+		},
+
+		{
+			"name"			: 		"Dal",
+			"image"			: 		"http://redchillycurry.com/wp-content/uploads/2012/08/gujarat-dal.jpg",
+			"description"	: 		"Cooked toor dal with Indian spices. Good source of protein",
+			"orderText"		: 		"",
+			"cost"			: 		1.50,
+			"costText"		: 		"per person",
+			"notesText"		: 		""
+		},
+
+		{
+			"name"			: 		"Bhaat",
+			"image"			: 		"http://www.manjulaskitchen.com/blog/wp-content/uploads/rice_white_plain.jpg",
+			"description"	: 		"Steam cooked rice. Perfect companion with Dal",
+			"orderText"		: 		"",
+			"cost"			: 		1.00,
+			"costText"		: 		"per person",
+			"notesText"		: 		""
+		}
+	];
+
+	return dinner;
+}
