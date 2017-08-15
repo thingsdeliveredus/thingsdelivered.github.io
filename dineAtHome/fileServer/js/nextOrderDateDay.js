96675ba4c58a4e5f072dbf1cdd768760be308ce5
thingsdeliveredus <thingsdeliveredus@users.noreply.github.com>
2017-08-15 15:02:11 -0400

    Update nextOrderDateDay.js

--- dineAtHome/fileServer/js/nextOrderDateDay.js
+++ dineAtHome/fileServer/js/nextOrderDateDay.js
@@ -1,5 +1,5 @@
 function getNextOrderDateDay() {
-	var nextOrder = new Date('2017-08-15');
+	var nextOrder = new Date('2017-08-17');
 	var todaysDate = new Date();
 
 	if (nextOrder == null || nextOrder <= todaysDate){
