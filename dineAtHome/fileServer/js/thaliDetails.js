0ac4b61249db798297693b2a1506aa477e6770ab
thingsdeliveredus <thingsdeliveredus@users.noreply.github.com>
2017-08-17 15:07:42 -0400

    Update thaliDetails.js

--- dineAtHome/fileServer/js/thaliDetails.js
+++ dineAtHome/fileServer/js/thaliDetails.js
@@ -1,7 +1,7 @@
 function getThaliDetail() {
 	var thali = 
 	{
-		"detail"	: "One order of thali includes 4 Roti, Potato Shak, Dal (toor), Bhat enough for one person.",
+		"detail"	: "One order of thali includes 4 Roti, Potato Shak, Dal (toor), Bhat enough for one person",
 		"cost"		: "7.99",
 		"costType"	: "per person"
 	}
