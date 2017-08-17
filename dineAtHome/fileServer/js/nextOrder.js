ea9c7cd8daad1451f1d7058e27ed485721c0d2cb
thingsdeliveredus <thingsdeliveredus@users.noreply.github.com>
2017-08-17 13:36:11 -0400

    Delete nextOrder.js

--- dineAtHome/fileServer/js/nextOrder.js
+++
@@ -1,9 +0,0 @@
-function getnextOrderDayDinner() {
-	var nextOrder = "Tomorrow";
-	return nextOrder;
-}
-
-function getnextOrderDayLunch() {
-	var nextOrder = "The Day After Tomorrow";
-	return nextOrder;
-}
