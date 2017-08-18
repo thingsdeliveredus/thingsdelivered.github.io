590354c7261a5701439488b89cb9b6dabd96a942
thingsdeliveredus <thingsdeliveredus@users.noreply.github.com>
2017-08-18 15:05:21 -0400

    Update dinnerDetails.js

--- dineAtHome/fileServer/js/dinnerDetails.js
+++ dineAtHome/fileServer/js/dinnerDetails.js
@@ -7,3 +7,28 @@ function getThaliDetail() {
 	}
 	return thali;
 }
+
+function getCustomizedDinnerDetail() {
+	var items = [
+		{
+			"item"		: "Roti",
+			"image"		: "http://www.supremequalityfoods.com.au/images/products/product-chapati-wholemeal.jpg",
+			"message"	: "Made from stoneground wholemeal flour, traditionally known as atta with thin layer of ghee on top",
+			"cost"		: "1.00",
+			"costType"	: "per pack (3 roties)",
+			"orderPreText"	: "Order",
+			"orderPostText"	: "packs"
+		},
+		
+		{
+			"item"		: "Potato Shak",
+			"image"		: "http://cdn3.foodviva.com/static-content/food-images/curry-recipes/batata-nu-shaak-gujarati-style-potato-subzi/batata-nu-shaak-gujarati-style-potato-subzi.jpg",
+			"message"	: "Cut potato cooked in spices",
+			"cost"		: "2.50",
+			"costType"	: "per person",
+			"orderPreText"	: "Order for",
+			"orderPostText"	: "person"
+		}
+	]
+	
+}
